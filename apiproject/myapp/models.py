d217478e996259b3645a759db671a5b80a21b49b
kothet <kothetnaingtun.ict@gmail.com>
2024-09-10 20:25:29 +0630

    Class Base APIView

--- apiproject/myapp/models.py
+++ apiproject/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from rest_framework import serializers
 # Create your models here.
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos']
